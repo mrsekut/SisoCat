34f51a64ae32a4579820085bb3a0812052ef36cb
mrsekut <k.cloudspider@gmail.com>
2021-02-05 00:37:05 +0900

    fix(daummy data): use Response Note Data

--- app/models/notes/hooks/useGetNote.ts
+++ app/models/notes/hooks/useGetNote.ts
@@ -1,26 +1,24 @@
 import { note0 } from 'app/components/Reditor/utils/dummy';
 import { useTextWidths } from 'app/models/Cursor';
-import { useQuery } from 'blitz';
-import getNote from '../queries/getNote';
 import { NoteM } from '../typings';
 
 export const useGetNote = (noteId: number): NoteM => {
-  return note0;
+  const note = note0;
   // const [note] = useQuery(getNote, { where: { id: 1 } });
-  // // const [user] = useQuery(getUser, { where: { id: note.userId } });
-  // const { textWidths } = useTextWidths();
+  // const [user] = useQuery(getUser, { where: { id: note.userId } });
+  const { textWidths } = useTextWidths();
 
-  // const lines = note.lines
-  //   .split('\n')
-  //   .map(line => ({ value: line, widths: textWidths(line) }));
+  const lines = note.lines
+    .split('\n')
+    .map(line => ({ value: line, widths: textWidths(line) }));
 
-  // return {
-  //   ...note,
-  //   author: {
-  //     id: 'user1',
-  //     name: 'mrsekut',
-  //   },
-  //   lines,
-  //   references: [],
-  // };
+  return {
+    ...note,
+    author: {
+      id: 'user1',
+      name: 'mrsekut',
+    },
+    lines,
+    references: [],
+  };
 };
