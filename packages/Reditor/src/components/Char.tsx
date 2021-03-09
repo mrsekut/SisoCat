91ca535ff312e758ade420e86a7cdf315c15dc96
mrsekut <k.cloudspider@gmail.com>
2021-03-10 02:47:17 +0900

    fix(reditor): input space

--- packages/Reditor/src/components/Char.tsx
+++ packages/Reditor/src/components/Char.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { x } from '@xstyled/styled-components';
 import { Pos } from '../Shared/typings';
 import { useFocus } from '../Cursor/useFocus';
+import { Space } from './Space';
 
 type Props = {
-  children: React.ReactChild;
+  children: string;
   pos: Pos;
 };
 
 export const Char: React.VFC<Props> = ({ children, pos }) => {
   const { focus } = useFocus();
+  const isSpace = /\s/.test(children);
+
   return (
     <x.span
       onClick={() => focus(pos)}
@@ -17,7 +20,7 @@ export const Char: React.VFC<Props> = ({ children, pos }) => {
       fontFamily='mono'
       lineHeight='snug'
     >
-      {children}
+      {isSpace ? <Space /> : children}
     </x.span>
   );
 };
